07897552cf1bb9bb2e0b4051674c2641529413fb
Ari Sosnovsky <ariel@sosnovsky.ca>
2023-01-05 22:05:45 -0500

    removed nasnode

--- infrastructure/meta.tf
+++ infrastructure/meta.tf
@@ -3,26 +3,3 @@ resource "kubernetes_namespace" "hl" {
     name = "homelab"
   }
 }
-
-resource "kubernetes_labels" "masternode" {
-  api_version = "v1"
-  kind        = "Node"
-  metadata {
-    name = var.node_name_master
-  }
-  labels = {
-    "homelab/master-node" = "yes"
-  }
-  field_manager = var.node_name_master
-}
-resource "kubernetes_labels" "nasnode" {
-  api_version = "v1"
-  kind        = "Node"
-  metadata {
-    name = var.node_name_nas
-  }
-  labels = {
-    "homelab/nas-node" = "yes"
-  }
-  field_manager = var.node_name_nas
-}
