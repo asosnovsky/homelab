3e4803366b680aea9b533d0c4684a2559d1a77e5
Ari Sosnovsky <ariel@sosnovsky.ca>
2023-02-18 21:41:57 +0000

    initial setup

---
+++ .devcontainer/local-features/tweaks/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+
+USERNAME="${USERNAME:-"${_REMOTE_USER}"}"
+
+cat >> /home/${USERNAME}/.zshrc <<- EOM
+source <(kubectl completion zsh)
+alias k=kubectl
+complete -o default -F __start_kubectl k
+source <(k3d completion zsh)
+source <(helm completion zsh)
+EOM
+
