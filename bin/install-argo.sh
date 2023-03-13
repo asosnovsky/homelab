5d5801cade06d930a8fd71cfababd66d08b25303
Ari Sosnovsky <ariel@sosnovsky.ca>
2023-03-13 22:39:19 +0000

    latest work

--- bin/install-argo.sh
+++ bin/install-argo.sh
@@ -17,3 +17,9 @@ watch -n1 "kubectl -n argocd get all"
 argocd login cd.argoproj.io --core
 kubectl config set-context --current --namespace argocd
 argocd admin initial-password
+
+helm upgrade argocd k8s/argocd \
+    --namespace argocd \
+    --create-namespace \
+    --set selfapp.enabled="true" \
+    --set selfapp.sshkey="$(cat ~/.ssh/id_rsa)"
