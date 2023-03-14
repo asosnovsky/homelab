eb5a403a8db60ed3c7d4cac28aa5e14418353697
Ari Sosnovsky <ariel@sosnovsky.ca>
2023-03-14 22:23:51 +0000

    fixed up longhorn;

---
+++ bin/dryrun-app.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+full_path=${1}
+deployment=${2:-dev}
+app=$(basename $full_path)
+location=$(dirname $full_path)
+tmp_path=$(realpath ".tmp/$location/$deployment-$app.yaml")
+echo "writing $location/$app to $tmp_path"
+
+mkdir -p $(dirname $tmp_path)
+
+pushd $full_path
+helm dependency update
+helm template $app . --namespace $app \
+    --debug \
+    --values values.yaml \
+    --values values-$deployment.yaml \
+    > $tmp_path
