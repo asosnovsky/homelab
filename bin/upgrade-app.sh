20befe6624f78c3aa7302f48a233dc085639439a
Ari Sosnovsky <ariel@sosnovsky.ca>
2023-03-14 22:40:28 +0000

    keep 1

--- bin/upgrade-app.sh
+++ bin/upgrade-app.sh
@@ -5,15 +5,20 @@ full_path=${1}
 deployment=${2:-dev}
 app=$(basename $full_path)
 location=$(dirname $full_path)
-tmp_path=$(realpath ".tmp/$location/$deployment-$app.yaml")
-echo "writing $location/$app to $tmp_path"
-
+tmp_path=".tmp/$location/$deployment-$app.yaml"
+argocd_values_path=$(realpath k8s/argocd/values-$deployment.yaml)
+echo "> writing $location/$app to $tmp_path"
+echo "> using argo values of $argocd_values_path"
+cat $argocd_values_path | yq
 mkdir -p $(dirname $tmp_path)
 
-pushd $full_path
-helm dependency update
-helm upgrade $app . --namespace $app \
+pushd $full_path &> /dev/null
+helm dependency update &> /dev/null
+popd &> /dev/null
+
+helm upgrade $app $full_path --namespace $app \
     --debug \
-    --values values.yaml \
-    --values values-$deployment.yaml \
+    --values $argocd_values_path \
+    --values $full_path/values.yaml \
+    --values $full_path/values-$deployment.yaml \
     > $tmp_path
