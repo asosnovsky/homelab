24941d09c690626e870784abef6047314e58702b
Ari Sosnovsky <ariel@sosnovsky.ca>
2024-02-03 14:04:21 -0500

    devenv

---
+++ devenv.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+{
+  packages = [ 
+    pkgs.git
+    pkgs.kubectl
+    pkgs.kubernetes-helm
+    pkgs.argocd
+  ];
+
+  languages.nix.enable = true;
+  # pre-commit.hooks.shellcheck.enable = true;
+}
