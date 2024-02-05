b2ad3f3269fe3127681377a86c5e37200c71299c
Ariel Sosnovsky <asosnovsky@sumologic.com>
2024-02-05 17:17:49 -0500

    router setup

--- devenv.nix
+++ devenv.nix
@@ -6,8 +6,13 @@
     pkgs.kubectl
     pkgs.kubernetes-helm
     pkgs.argocd
+    # pkgs.terraform
   ];
 
   languages.nix.enable = true;
+  languages.terraform.enable = true;
+  languages.python.enable = true;
+  languages.python.venv.enable = true;
+  languages.python.venv.requirements = ./pydeps.requirements;
   pre-commit.hooks.shellcheck.enable = true;
 }
