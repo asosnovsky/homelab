092c16b6156ccf69aa805fc86637a8085ca955ad
Ariel Sosnovsky <asosnovsky@sumologic.com>
2024-02-06 12:17:35 -0500

    caps & summary

--- os/routers/glmain/gen-mappings.py
+++ os/routers/glmain/gen-mappings.py
@@ -7,7 +7,7 @@ BASE_PATH = Path(__file__).parent
 CONFIG_FILE = BASE_PATH / "config.yaml"
 OUTPUT_FOLDER = BASE_PATH / "output"
 DNSMASQ_CONF = OUTPUT_FOLDER / "var" / "dnsmasq.conf"
-
+SUMMARY = OUTPUT_FOLDER / "summary.yaml"
 DNSMASQ_CONF.parent.mkdir(exist_ok=True, parents=True)
 
 root_domain = "internal"
@@ -18,19 +18,31 @@ ip_prefix_map = {
     "cam": "10.0.13.",
 }
 
+def cap1_and_join(*args: str) -> str:
+    out = ""
+    for a in args:
+        out += a[0].upper()
+        out += a[1:].lower()
+    return out
+
 with CONFIG_FILE.open('r') as fp:
     devices = yaml.safe_load(fp)
 
-with DNSMASQ_CONF.open('w') as fp:
+with DNSMASQ_CONF.open('w') as dfp, SUMMARY.open('w') as sfp:
     for sub_domain, configs in devices.items():
         ip_prefix = ip_prefix_map[sub_domain]
+        sfp.write(f'\n{cap1_and_join(sub_domain)}:')
         for ip_suffix, dvc in enumerate(configs, start=1):
             mac = dvc['mac']
             name = dvc['name']
+            sfp.write(f'\n   {cap1_and_join(name)}:')
             ip = f"{ip_prefix}{ip_suffix}"
             host = f"{name}.{sub_domain}.{root_domain}"
-            fp.write(f"dhcp-host={mac},{name}{sub_domain},{ip},infinite\n")
-            fp.write(f"address=/{host}/{ip}\n")
+            sfp.write(f'\n       ip: "{ip}"')
+            sfp.write(f'\n       host: "{host}"')
+            dfp.write(f"dhcp-host={mac},{cap1_and_join(sub_domain, name)},{ip},infinite\n")
+            dfp.write(f"address=/{host}/{ip}\n")
             if domain := dvc.get('domain'):
-                fp.write(f"address=/{domain}/{ip}\n")
-                fp.write(f"address=/.{domain}/{ip}\n")
+                dfp.write(f"address=/{domain}/{ip}\n")
+                dfp.write(f"address=/.{domain}/{ip}\n")
+                sfp.write(f'\n       domain: "{domain}"')
