a93a3085280f20385e5ac0905296919f07f0491d
Ariel Sosnovsky <asosnovsky@sumologic.com>
2024-02-06 22:04:19 -0500

    bootloader

--- os/nixos/main.nix
+++ os/nixos/main.nix
@@ -18,8 +18,8 @@
     ];
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
   networking.hostName = hostName;
   system.stateVersion = stateVersion;
 }
