272664f0f9678fcedc5c8fa235a6bff2ebf5933f
Ari <ari@me.ca>
2024-03-21 11:58:47 -0400

    more amd support

--- os/nixos/amd-packages.nix
+++ os/nixos/amd-packages.nix
@@ -3,6 +3,16 @@
   environment.systemPackages = with pkgs; [
      rocmPackages.rocm-smi
      rocmPackages.rpp     
+     rocmPackages.rocm-core
+     rocmPackages.rocm-runtime
+     rocmPackages.hipblas
+     rocmPackages.llvm.clang
+     amdgpu_top
+     amdctl
   ];
   boot.initrd.kernelModules = [ "amdgpu" ];
+  hardware.opengl.extraPackages = with pkgs; [
+  	rocmPackages.clr.icd
+	amdvlk
+  ];
 }
