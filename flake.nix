12b240adf43c6c6b110e72044a981350c87ab61f
Ari Sosnovsky <ariel@sosnovsky.ca>
2025-02-21 21:20:27 -0500

    using flakes

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    futils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    { self, nixpkgs, futils }: (
+      futils.lib.eachDefaultSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+        in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              # utilities
+              yq
+              bat
+              jq
+              # k8s 
+              k3d
+              (wrapHelm kubernetes-helm {
+                plugins = with pkgs.kubernetes-helmPlugins; [
+                  helm-diff
+                ];
+              })
+              helmfile-wrapped
+              argocd
+              yq
+              jq
+            ];
+            shellHook = ''
+              echo "$(pwd)"
+              export PATH="$(pwd)/bin:$PATH"
+              export PROMPT="🏠> $PROMPT"
+              echo "Welcome to HomeLab!"
+            '';
+          };
+        }
+      )
+    );
+}
